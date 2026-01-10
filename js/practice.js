50776088cee0e7e96a1e849bca09b6dab81d9453
ZulfiqarAlam18 <zulfi18.official@gmail.com>
2026-01-10 14:08:20 +0500

    more practice

--- js/practice.js
+++ js/practice.js
@@ -5,4 +5,44 @@ for (let i = 1; i<=100; i++){
     sum += i;
 
 }
+
 console.log(sum);
+
+
+// problem 2
+
+let factorial = 1;
+
+for (let i = 1; i<=5; i++){
+
+    factorial *= i;
+
+}
+
+console.log(factorial);
+
+// problem 3
+
+for (let i = 1; i<=10; i++){
+
+    if (i % 2 !== 0){
+
+        console.log(i);
+
+    }
+
+}
+
+// problem 4
+
+for (let i = 1; i<=10; i++){
+
+    if (i % 2 === 0){
+
+        console.log(i);
+
+    }
+
+}
+
+// problem 5
