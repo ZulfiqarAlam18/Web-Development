3ed7d0274faf4fdb4d17bea98bddfc151ffcfabf
ZulfiqarAlam18 <zulfi18.official@gmail.com>
2026-01-11 18:04:50 +0500

    string methods studies

---
+++ js/string_methods.js
@@ -0,0 +1,18 @@
+
+let name = "  Zulfiqar Alam Jamali    ";
+
+
+console.log(name);
+
+console.log(name.length);
+console.log(name.replace('Jamali','Jamali Sahab'));
+console.log(name.trim());
+console.log(name.slice(0,10));
+//console.log(name.reverse());
+console.log(name.charAt(3));
+console.log(name.indexOf('A'));
+console.log(name.includes('Alam'));
+console.log(name.toUpperCase());
+console.log(name.length);
+console.log(name.length);
+
