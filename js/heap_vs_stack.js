af9317cf63bb7eb66e8248b426df62ab4a7e5ab4
ZulfiqarAlam18 <zulfi18.official@gmail.com>
2026-01-11 15:35:08 +0500

    stack  vs heap

---
+++ js/heap_vs_stack.js
@@ -0,0 +1,31 @@
+let a = 10;
+
+let b = a;
+
+b  =  30;
+
+
+console.log(a); // 10
+console.log(b); // 20
+
+
+let user1  = {
+
+    name : "Zulfi"
+
+}
+
+let user2 = user1;
+
+
+user2.name = "Zulfiqar";
+
+console.log(user1); // Zulfiqar
+console.log(user2) // Zulfiqar
+
+
+// stack => primitive
+
+// head => reference/non primitive
+
+
