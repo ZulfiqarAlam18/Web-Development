61ade05764891835b1aa881c182db72dc90b9f49
ZulfiqarAlam18 <zulfi18.official@gmail.com>
2026-01-11 17:55:08 +0500

    string fundamentals

---
+++ js/strings_01.js
@@ -0,0 +1,29 @@
+let a = "Zulfiqar"
+
+let b = 'Alam'
+
+// let c = """zulfi"""; // error
+
+console.log(typeof(a));
+console.log(typeof(b))
+
+
+// concatination
+
+let fullName = a + " " + b;
+
+console.log(fullName);
+
+// string interpolation
+
+let fullName2 = `${a} ${b} is a good boy`;
+
+console.log(fullName2);
+
+
+console.log(`My Name is ${a} and Name of my father is ${b}`)
+
+
+const title = new String('Zulfiqar')
+
+console.log(title);
