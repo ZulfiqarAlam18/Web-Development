54b20dbbca409be1727759e918a6330d0f83d317
ZulfiqarAlam18 <zulfi18.official@gmail.com>
2026-04-14 10:20:20 +0500

    datatypes and variables

---
+++ js_basics/dataypes_varaibles/datatype.js
@@ -0,0 +1,44 @@
+// primitive
+
+
+let name = "Zulfi";
+
+let age = 20;
+
+let weight = 65.5;
+
+let gender = 'm';
+
+let null_var = null;
+
+let undefined_var ;
+
+let symbol_var = Symbol();
+
+
+
+// non primitive 
+
+
+// 1. objects 2. Lists 3.Functions
+
+
+let obj = {
+
+    "name": "Zulfi",
+    "age": 19
+
+
+}
+
+let list = [2,3,5];
+
+
+function greet(){
+
+    console.log('hello');
+
+
+}
+
+console.log(obj)
---
+++ js_basics/dataypes_varaibles/key_difference.js
@@ -0,0 +1,32 @@
+// data 
+
+
+let a = 10;
+
+var b = 10; // avoid 
+
+const c = 10;
+
+
+
+a = 20;
+
+b = 20;
+
+//c = 20;
+
+try {
+
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+}
+catch (e){
+
+    console.log(e);
+
+}
+
+console.log('Debug line .............');
